caab936b9a7d6d0869b97ecf00b851caa7b56e69
andr3w1000 <andr3w1000@gmail.com>
2026-05-05 14:41:22 +0200

    update notebook

--- notebooks/load_click_data.py
+++ notebooks/load_click_data.py
@@ -2,7 +2,6 @@
 from src.utils.logger import get_logger, flush_logger
 from src.readers.stream_reader import read_stream_data
 from src.writers.stream_writer import write_stream_data
-from src.utils.schemas import CLICK_EVENT_SCHEMA
 
 # COMMAND ----------
 
@@ -13,17 +12,23 @@ logger.info("Starting click data load")
 
 # DBTITLE 1,Cell 3
 job_config = {
-    "read_format": "cloudFiles",
-    "source_location": "/Volumes/cda_dev/clickengine/test_data/click",
+    "read_format": "kafka",
     "read_options": {
-        "cloudFiles.format": "json",
-        "cloudFiles.schemaLocation": "/Volumes/cda_dev/clickengine/schemas/click",
-        "multiLine": "true",
+        "kafka.bootstrap.servers": "evhns-cda-dev.servicebus.windows.net:9093",
+        "subscribe": "evh-clicks",
+        "kafka.security.protocol": "SASL_SSL",
+        "kafka.sasl.mechanism": "OAUTHBEARER",
+        "kafka.sasl.jaas.config": (
+            "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required;"
+        ),
+        "kafka.sasl.login.callback.handler.class": (
+            "com.microsoft.azure.eventhubs.kafka.oauthbearer.OAuthBearerLoginCallbackHandler"
+        ),
+        "startingOffsets": "latest",
     },
-    "schema": CLICK_EVENT_SCHEMA,
     "checkpoint_location": "/Volumes/cda_dev/clickengine/checkpoints/load_click/checkpoint",
     "target_table": "cda_dev.clickengine.clicks",
-    "mode": "append",
+    "output_mode": "append",
     "trigger": {"availableNow": True},
     "write_format": "delta"
 }
